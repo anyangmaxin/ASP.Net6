ef3c648d2936bc15cf46f867899ee67601f0963a
马鑫 <anyangmaxin@163.com>
2022-08-31 00:10:17 +0800

    MiddleWare第一个中间件测试  只能处理localhost的请求

--- ASP.Net6/MiddlewareDemo/Program.cs
+++ ASP.Net6/MiddlewareDemo/Program.cs
@@ -4,7 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+/**
+ * Map是接待客人(请求)的作用：告诉对应的请求，我这里可以处理 Use 是中间件，每一个Map就是一道门的守门员，门后面的功能由各个不同的中间件组成
+ */
 
+//只能处理localhost的请求
+app.MapGet("/", () => "Hello World");
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -16,10 +21,16 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+//路由中间件
 app.UseRouting();
 
+//授权中间件
 app.UseAuthorization();
 
 app.MapRazorPages();
 
+
+
 app.Run();
+
+
