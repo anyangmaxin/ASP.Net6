bac50fae866a229a3233a4951b2a5d753886317a
马鑫 <anyangmaxin@163.com>
2022-08-31 10:24:14 +0800

    修改git配置文件

--- ASP.Net6/MiddlewareDemo/Program.cs
+++ ASP.Net6/MiddlewareDemo/Program.cs
@@ -4,12 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-/**
- * Map是接待客人(请求)的作用：告诉对应的请求，我这里可以处理 Use 是中间件，每一个Map就是一道门的守门员，门后面的功能由各个不同的中间件组成
- */
 
-//只能处理localhost的请求
-app.MapGet("/", () => "Hello World");
+
+
+
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -29,7 +28,34 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+/**
+ * Map是接待客人(请求)的作用：告诉对应的请求，我这里可以处理 Use 是中间件，每一个Map就是一道门的守门员，门后面的功能由各个不同的中间件组成
+ */
 
+#region 中间件测试
+
+#region 第一个简单的中间件
+//只能处理localhost的请求
+//app.MapGet("/", () => "Hello World");
+#endregion
+
+#region 第二种测试中间件
+//app.Map("/test", pilebuilder =>
+//{
+//    pilebuilder.Use(async (context, next) =>
+//    {
+//        //前代码
+//        await context.Response.WriteAsync("Hello World 1 \r\n");
+//        //await next.Invoke();
+//        await next();
+//        //后代码
+//        await context.Response.WriteAsync("Hellow World 1 End \r\n");
+//    });
+//});
+#endregion
+
+
+#endregion
 
 app.Run();
 
