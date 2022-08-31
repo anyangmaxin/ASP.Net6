839114872f671e4ed63cd4dc75eb82df673f48c3
马鑫 <anyangmaxin@163.com>
2022-08-31 22:47:30 +0800

    测试Filter项目展示异常信息

---
+++ ASP.Net6/FilterDemoWebAPI/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilterDemoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string Test1()
+        {
+            string s = System.IO.File.ReadAllText("a.txt");
+            return s;
+        }
+    }
+}
