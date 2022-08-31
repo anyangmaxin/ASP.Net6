978ed88ede16913c8a0d7ea9d2e0ffc5a406f615
马鑫 <anyangmaxin@163.com>
2022-08-31 16:35:11 +0800

    执行了run 以后，后面的代码不会再执行

--- ASP.Net6/MiddlewareDemo/Program.cs
+++ ASP.Net6/MiddlewareDemo/Program.cs
@@ -7,29 +7,6 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-
-
-
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-//路由中间件
-app.UseRouting();
-
-//授权中间件
-app.UseAuthorization();
-
-app.MapRazorPages();
-
 /**
  * Map是接待客人(请求)的作用：告诉对应的请求，我这里可以处理 Use 是中间件，每一个Map就是一道门的守门员，门后面的功能由各个不同的中间件组成
  */
@@ -40,10 +17,10 @@ app.MapRazorPages();
 //只能处理localhost的请求
 //app.MapGet("/", () => "Hello World");
 
-//app.Run(async context =>
-//{
-//    await context.Response.WriteAsync("Hello World \r\n");
-//});
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello World \r\n");
+});
 #endregion
 
 #region 第二种测试中间件
@@ -82,6 +59,28 @@ app.MapRazorPages();
 #endregion
 
 #endregion
+
+//因为执行了run 所以下面的代码不会被 执行。
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+//路由中间件
+app.UseRouting();
+
+//授权中间件
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 
 
