282aa0a2e6fc5f0b833e642001dcefba2cd8c84f
马鑫 <anyangmaxin@163.com>
2022-09-14 10:08:58 +0800

    测试缓存

---
+++ ASP.Net6/TestConsole/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestConsole;
+
+Console.WriteLine("Hello, World!");
+var a = new TestCache();
+a.SetCache("123", "456");
+Console.WriteLine(a.GetCache("123"));
---
+++ ASP.Net6/TestConsole/TestCache.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace TestConsole
+{
+    public class TestCache
+    {
+        MemoryCache memoryCache = new MemoryCache(new MemoryCacheOptions());
+
+        public bool SetCache(string key,string value)
+        {            
+            memoryCache.Set(key, value); 
+            return true;
+        }
+
+        public object GetCache(string key)
+        {
+            return memoryCache.Get(key);
+        }
+
+     
+    }
+}
